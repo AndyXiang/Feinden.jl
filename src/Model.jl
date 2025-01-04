53b9dabee34fe6cf33309e6e4dfb777b8d3c7002
AndyXiang <xjy03@hust.edu.cn>
2025-01-04 20:55:05 +0800

    style Model.jl & QED.json > change of field name
    Some fields of structs are changed:
    1. Interaction: ids -> comb
    2. Model: fields (Vector{Field}) -> field (Dict) , coupling -> interaction.
    Construct functions of Fields are deleted, since their instance will be created only when model is initiated.
    Field dict of Model is deleted to minimize complexity.

--- src/Model.jl
+++ src/Model.jl
@@ -6,7 +6,6 @@ using JSON
 
 """
     Field 
-Abstract type for fields.
 """
 abstract type Field end
 
@@ -28,7 +27,7 @@ struct GhostField <: Field
 end
 
 struct Interaction
-    ids::Vector{Int}
+    comb::Vector{Int}
     symbol::Union{String, Nothing}
 end
 
@@ -36,171 +35,174 @@ end
     Model 
 """
 struct Model
-    fields::Vector{Field}
-    couplings::Vector{Interaction}
-    dict::Dict
+    field::Dict
+    interaction::Vector{Interaction}
     desc::Union{String, Nothing}
 end
 
-# Construction functions 
-
-function ScalarField(id::Int, model::Model = CURRENT_MODEL)
-    if (haskey(model.dict["spin"], id)) && (model.dict["spin"][id] == 0)
-        return ScalarField(id, model.dict["name"][id])
-    else 
-        throw(ArgumentError("Invaild id for a scalar field in current model."))
-    end
-end
-
-function SpinorField(id::Int, model::Model = CURRENT_MODEL)
-    if (haskey(model.dict["spin"], id)) && (model.dict["spin"][id] == 1//2)
-        return SpinorField(id, model.dict["name"][id])
-    else 
-        throw(ArgumentError("Invaild id for a spinor field in current model."))
-    end
-end
-
-function VectorField(id::Int, model::Model = CURRENT_MODEL)
-    if (haskey(model.dict["spin"], id)) && (model.dict["spin"][id] == 1)
-        return VectorField(id, model.dict["name"][id])
-    else 
-        throw(ArgumentError("Invaild id for a vector field in current model."))
-    end
-end
-
 Model(
-    fields::Vector{Field}, 
-    couplings::Vector{Interaction}, 
-    dict::Dict
-) = Model(fields, couplings, dict, nothing)
+    field::Vector{Field}, 
+    interaction::Vector{Interaction}
+) = Model(field, interaction, nothing)
 
-###########################################################################################
+
+# Construction functions 
+# function ScalarField(id::Int, model::Model = CURRENT_MODEL)
+#     if (haskey(model.field, ScalarField)) && ()
+#         return ScalarField(id, model.dict["name"][id])
+#     else 
+#         throw(ArgumentError("Invaild id for a scalar field."))
+#     end
+# end
+
+# function SpinorField(id::Int, model::Model = CURRENT_MODEL)
+#     if (haskey(model.dict["spin"], id)) && (model.dict["spin"][id] == 1//2)
+#         return SpinorField(id, model.dict["name"][id])
+#     else 
+#         throw(ArgumentError("Invaild id for a spinor field in current model."))
+#     end
+# end
+
+# function VectorField(id::Int, model::Model = CURRENT_MODEL)
+#     if (haskey(model.dict["spin"], id)) && (model.dict["spin"][id] == 1)
+#         return VectorField(id, model.dict["name"][id])
+#     else 
+#         throw(ArgumentError("Invaild id for a vector field in current model."))
+#     end
+# end
+
 
 # APIs
-
 function load_model(path::String) 
     json_dict = JSON.parsefile(path) # read model file from .json and store in Dict 
-    # official consist of three keys: "fields", "couplings", "dict", "desc" may exist.
-    model_dict = Dict(
-        "mass" => Dict(), 
-        "spin" => Dict(), 
-        "name" => Dict()
-    )
-    fields = Vector{Field}()
-    for (k, v) in json_dict["fields"]
+    # official consist of three keys: "fields", "interactions", "dict", "desc" may exist.
+    # model_dict = Dict(
+    #     "mass" => Dict(), 
+    #     "spin" => Dict(), 
+    #     "name" => Dict()
+    # )
+    field_dict = Dict()
+    for (k, v) in json_dict["field"]
         # k is field type, v is vectors of [id::Int, name::String]
-        for field in v
-            push!(fields, NAME_TO_FIELD_TYPE[k](field[1], field[2]))
-            model_dict["mass"][field[1]] = json_dict["mass"][field[2]]
-            model_dict["spin"][field[1]] = NAME_TO_SPIN[k]
-            model_dict["name"][field[1]] = field[2]
+        for field_param in v
+            field_datatype = FIELD_NAME_TO_FIELD_DATATYPE[k]
+            if haskey(field_dict, field_datatype)
+                push!(field_dict[field_datatype], field_datatype(field_param[1], field_param[2]))
+            else 
+                field_dict[field_datatype] = [field_datatype(field_param[1], field_param[2])]
+            end
         end
     end
-    couplings = Vector{Interaction}()
-    for cp in json_dict["couplings"]
-        push!(couplings, Interaction(cp[1], cp[2]))
+    interaction_arr = Vector{Interaction}()
+    for cp_param in json_dict["interaction"]
+        push!(interaction_arr, Interaction(cp_param[1], cp_param[2]))
     end
     haskey(json_dict, "desc") ? 
-        (return Model(fields, couplings, model_dict, json_dict["desc"])) : 
-        (return Model(fields, couplings, model_dict))
+        (return Model(field_dict, interaction_arr, json_dict["desc"])) : 
+        (return Model(field_dict, interaction_arr, nothing))
 end
 
 function load_model!(path::String, model::Model) 
-    model = load_model(path) 
+    new_model = load_model(path)
+    model.field = new_model.field 
+    model.interaction = new_model.interaction
+    model.desc = new_model.desc
+    return model
 end 
 
 function save_model(path::String, model::Model)
     json_dict = Dict(
         "field" => Dict(), 
-        "couplings" => Vector(), 
-        "mass" => Dict()
+        "interaction" => Vector()
     )
     if !(isnothing(model.desc))
         json_dict["desc"] = model.desc
     end 
-    for field in model.fields
-        field_type = FIELD_TYPE_TO_NAME[typeof(field)]
-        if haskey(json_dict["field"], field_type)
-            push!(json_dict["field"][field_type], [field.id, field.name])
+    for (field_type, field_instance) in model.fields
+        field_type_name = FIELD_DATATYPE_TO_FIELD_NAME[field_type]
+        if haskey(json_dict["field"], field_type_name)
+            push!(json_dict["field"][field_type_name], [field_instance.id, field_instance.name])
         else 
-            json_dict["field"][field_type] = Vector()
-            push!(json_dict["field"][field_type], [field.id, field.name])
+            json_dict["field"][field_type_name] = [[field_instance.id, field_instance.name]]
         end
-        json_dict["mass"][field.name] = model.dict["mass"][field.id] 
     end
-    for cp in model.couplings
-        push!(json_dict["couplings"], [cp.ids, cp.symbol])
+    for cp_instance in model.interaction
+        push!(json_dict["interaction"], [cp_instance.comb, cp_instance.symbol])
     end
     open(path, "w") do f 
         write(f, JSON.json(json_dict, 4))
     end
 end
 
+"""
+    getid 
+Return the field's id with the given name.
+"""
 function getid(name::String, model::Model = CURRENT_MODEL)
-    for (k, v) in model.dict["name"]
-        if v == name 
-            return k 
+    for field_arr in values(model.field)
+        for field in field_arr
+            if field.name == name 
+                return field.id 
+            end
+        end
+    end
+    throw(ArgumentError("No field with this name in the model."))
+end
+
+"""
+    getparticle
+Return the instance of field with the given id.
+"""
+function getparticle(id::Int, model::Model = CURRENT_MODEL)
+    for field_arr in values(model.field)
+        for field in field_arr
+            if field.id == id 
+                return field 
+            end 
+        end 
+    end
+end
+getparticle(name::String, 
+    model::Model = CURRENT_MODEL
+) = getparticle(getid(name, model), model)
+
+getname(field::Field) = isnothing(field.name) ? println("Name not assigned") : field.name
+getname(id::Int, model::Model = CURRENT_MODEL) = getname(getparticle(id, model))
+
+function fieldlist(model::Model = CURRENT_MODEL)
+    println("id\t\t", "field type\t\t", "name")
+    for field_arr in values(model.field)
+        for field in field_arr
+            println(
+                field.id, "\t\t", 
+                split(string(typeof(field)), ".")[end], "\t\t", 
+                isnothing(field.name) ? "not assigned" : field.name
+            )
         end
     end
 end
 
-getidabs(name::String, model::Model = CURRENT_MODEL) = abs(getid(name, model))
+# Base.hash
+Base.hash(field::Field) = hash(field.id, hash(field.name))
+Base.hash(coup::Interaction) = hash(coup.comb, hash(coup.symbol))
+Base.hash(model::Model) = hash(model.field, hash(model.interaction))
 
-function getanti(field::Field, model::Model = CURRENT_MODEL) 
-    if model.dict["charge"][field.id] != 0
-        return getfield(-field.id)
-    else 
-        return field
-    end
-end
-
-function getfield(id::Int, model::Model = CURRENT_MODEL)
-    for field in model.fields
-        if field.id == id 
-            return field 
-        end 
-    end
-end
-
-getname(field::Field) = isnothing(field.name) ? println("Name not assigned") : field.name
-getname(id::Int, model::Model = CURRENT_MODEL) = model.dict["name"][id]
-
-getmass(id::Int, model::Model = CURRENT_MODEL) = model.dict["mass"][id]
-getmass(field::Field, model::Model = CURRENT_MODEL) = getmass(field.id, model)
-getmass(name::String, model::Model = CURRENT_MODEL) = getmass(getid(name, model), model)
-
-getcharge(id::Int, model::Model = CURRENT_MODEL) = model.dict["charge"][id]
-getcharge(field::Field, model::Model = CURRENT_MODEL) = getcharge(field.id, model)
-getcharge(name::String, model::Model = CURRENT_MODEL) = getcharge(getid(name, model), model)
-
-function fieldlist(model::Model = CURRENT_MODEL)
-    println("id\t\t", "field type\t\t", "name")
-    for field in model.fields
-        println(
-            field.id, "\t\t", 
-            split(string(typeof(field)), ".")[end], "\t\t", 
-            isnothing(field.name) ? "not assigned" : field.name
-        )
-    end
-end
-
-
+# Base.show
 
 # Dictionary
-
-const NAME_TO_FIELD_TYPE = Dict(
+const FIELD_NAME_TO_FIELD_DATATYPE = Dict(
     "scalar" => ScalarField,
     "spinor" => SpinorField,
     "vector" => VectorField,
     "ghost" => GhostField
 )
-const FIELD_TYPE_TO_NAME = Dict(
+const FIELD_DATATYPE_TO_FIELD_NAME = Dict(
     ScalarField => "scalar",
     SpinorField=> "spinor",
     VectorField => "vector",
     GhostField => "ghost"
 )
-const NAME_TO_SPIN = Dict(
+const FIELD_NAME_TO_SPIN = Dict(
     "scalar" => 0,
     "spinor" => 1//2,
     "vector" => 1
